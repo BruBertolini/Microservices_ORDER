089961eda630852f300991afc11418c43d1455ff
Bruna Bertolini <bbcorreia12@hotmail.com>
2019-04-14 17:32:06 -0300

    Add Swagger

--- src/main/java/br/com/fiap/orderservice/bean/Item.java
+++ src/main/java/br/com/fiap/orderservice/bean/Item.java
@@ -1,5 +1,6 @@
 package br.com.fiap.orderservice.bean;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -11,8 +12,15 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class Item {
     
+    @ApiModelProperty(notes = "Item ID")
     private int id;
+    
+    @ApiModelProperty(notes = "Item description")
     private String description;
+ 
+    @ApiModelProperty(notes = "Item price")
     private BigDecimal price;
+    
+    @ApiModelProperty(notes = "Quatity itens")
     private BigDecimal quantity  = BigDecimal.ZERO;
 }
