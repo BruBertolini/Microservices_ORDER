3390eaf73ba8d3e6ef75c25c2f6da929f44d11c8
Bruna Bertolini <bbcorreia12@hotmail.com>
2019-04-14 17:38:14 -0300

    Update Payment.java

--- src/main/java/br/com/fiap/orderservice/bean/Payment.java
+++ src/main/java/br/com/fiap/orderservice/bean/Payment.java
@@ -1,6 +1,8 @@
 package br.com.fiap.orderservice.bean;
 
 import lombok.*;
+import io.swagger.annotations.ApiModelProperty;
+
 
 @Getter
 @Setter
@@ -9,8 +11,15 @@ import lombok.*;
 @AllArgsConstructor
 public class Payment {
 
+    @ApiModelProperty(notes = "Payment ID")
     private int id;
+    
+    @ApiModelProperty(notes = "Card number", required="true")
     private String cardNumber;
+    
+    @ApiModelProperty(notes = "Card expiration date", required="true")
     private String expireDate;
+    
+    @ApiModelProperty(notes = "Card operator (Visa, Master, etc)", required="true")
     private String operator;
 }
